65c945054511b871500593a41e331d3e4ddc87c9
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-02-17 00:34:35 +0530

    Time: 30 ms (40.77%) | Memory: 25.7 MB (21.71%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-	int numsqhelper(string& s, string& t, int i, int j, vector<vector<int>>& dp){
-		if(i==0 || j==0){
-			return 0;
-		}
-		if(dp[i][j] != -1){
-			return dp[i][j];
-		}
-		int take = 0;
-		if(s[i-1] == t[j-1]){
-			take = 1 + numsqhelper(s,t,i-1,j-1, dp);
-		}
-		int nottake = max(numsqhelper(s,t,i-1,j, dp), numsqhelper(s,t,i,j-1, dp));
-		return dp[i][j] = max(take,nottake);
-	}
-
     int longestCommonSubsequence(string text1, string text2) {
-    	vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1,-1));
-        return numsqhelper(text1,text2,text1.size(),text2.size(), dp);
+    	vector<vector<int>> dp(text1.size()+1, vector<int>(text2.size()+1,0));
+    	for(int i=1; i<=text1.size(); i++){
+    		for(int j=1; j<=text2.size(); j++){
+				int take = 0;
+				if(text1[i-1] == text2[j-1]){
+					take = 1 + dp[i-1][j-1];
+				}
+				int nottake = max(dp[i-1][j],dp[i][j-1]);
+				dp[i][j] = max(take,nottake);
+    		}
+    	}
+        return dp[text1.size()][text2.size()];
     }
 };
