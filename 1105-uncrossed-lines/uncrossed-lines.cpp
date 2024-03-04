b9f7a958c4af5720f0fbf7242c54280f464671bb
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-03-05 01:34:13 +0530

    Time: 8 ms (73.85%) | Memory: 15.4 MB (17.40%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-
-	int counthelper(vector<int>& nums1, vector<int>& nums2, int i, int j, vector<vector<int>>& dp){
-		if(i<0 || j<0){
-			return 0;
-		}
-		if(dp[i][j] != -1){
-			return dp[i][j];
-		}
-		if(nums1[i]==nums2[j]){
-			return dp[i][j] = counthelper(nums1,nums2,i-1,j-1, dp) + 1;
-		}
-		return dp[i][j] = max(counthelper(nums1,nums2,i-1,j, dp),counthelper(nums1,nums2,i,j-1, dp));
-	}
-
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-    	vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size(),-1));
-        return counthelper(nums1,nums2,nums1.size()-1,nums2.size()-1, dp);
+    	vector<vector<int>> dp(nums1.size()+1, vector<int>(nums2.size()+1,0));
+    	for(int i=1; i<=nums1.size(); i++){
+    		for(int j=1; j<=nums2.size(); j++){
+    			if(nums1[i-1]==nums2[j-1]){
+    				dp[i][j] = dp[i-1][j-1] + 1;
+    			}else{
+    				dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+    			}
+    		}
+    	}
+        return dp[nums1.size()][nums2.size()];
     }
 };
