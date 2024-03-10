7de7ba11efd840bfc8e91134995cadb53152b4f6
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-03-10 23:24:24 +0530

    Time: 17 ms (56.06%) | Memory: 18.2 MB (27.88%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,38 +1,37 @@
 
 class Solution {
 public:
-	bool hasCycle(vector<vector<int>>& adjlist, int cur, vector<bool>& visited, vector<bool>& pathvisit){
+
+	bool dfstrav(int cur, vector<bool>& visited, vector<bool>& pathvisit, vector<vector<int>>& adjlist){
 		visited[cur] = 1;
-		pathvisit[cur]=1;
+		pathvisit[cur] = 1;
 		for(int i=0; i<adjlist[cur].size(); i++){
-			if(!visited[adjlist[cur][i]]){
-				if(hasCycle(adjlist,adjlist[cur][i],visited,pathvisit)){
-                    return 1;
-                };
+			if(pathvisit[adjlist[cur][i]]){
+				return 0;
 			}
-			if(visited[adjlist[cur][i]] && pathvisit[adjlist[cur][i]]){
-				return 1;
+			if(!visited[adjlist[cur][i]]){
+				if(!dfstrav(adjlist[cur][i],visited,pathvisit,adjlist)){
+					return 0;
+				}
 			}
 		}
-		pathvisit[cur]=0;
-		return 0;
+		pathvisit[cur] = 0;
+		return 1;
 	}
 
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjlist(n,vector<int>());
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> visited(numCourses, 0),pathvisit(numCourses,0);
+        vector<vector<int>> adjlist(numCourses,vector<int>());
         for(int i=0; i<prerequisites.size(); i++){
         	adjlist[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
-        vector<bool> pathvisit(n,0);
-        vector<bool> visited(n,0);
-        for(int i=0; i<visited.size(); i++){
+        for(int i=0; i<numCourses; i++){
         	if(!visited[i]){
-		        if(hasCycle(adjlist,i,visited,pathvisit)){
-		        	return 0;
-		        }
+        		if(!dfstrav(i,visited,pathvisit,adjlist)){
+        			return 0;
+        		}
         	}
         }
         return 1;
     }
 };
-
