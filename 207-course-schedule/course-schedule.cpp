8a5f14e1018cd9d1456926b8dc8e1a8f77c4bf76
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-03-13 11:10:57 +0530

    Time: 23 ms (19.51%) | Memory: 17 MB (68.89%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,39 @@
 
 class Solution {
 public:
-
-	bool dfstrav(int cur, vector<bool>& visited, vector<bool>& pathvisit, vector<vector<int>>& adjlist){
-		visited[cur] = 1;
-		pathvisit[cur] = 1;
-		for(int i=0; i<adjlist[cur].size(); i++){
-			if(pathvisit[adjlist[cur][i]]){
-				return 0;
-			}
-			if(!visited[adjlist[cur][i]]){
-				if(!dfstrav(adjlist[cur][i],visited,pathvisit,adjlist)){
-					return 0;
-				}
-			}
-		}
-		pathvisit[cur] = 0;
-		return 1;
-	}
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<bool> visited(numCourses, 0),pathvisit(numCourses,0);
+        vector<int> ans;
+        vector<int> indeg(numCourses,0);
+        queue<int> pending;
         vector<vector<int>> adjlist(numCourses,vector<int>());
         for(int i=0; i<prerequisites.size(); i++){
         	adjlist[prerequisites[i][0]].push_back(prerequisites[i][1]);
         }
+        for(int i=0; i<adjlist.size(); i++){
+        	for(int j=0; j<adjlist[i].size(); j++){
+        		indeg[adjlist[i][j]]++;
+        	}
+        }
         for(int i=0; i<numCourses; i++){
-        	if(!visited[i]){
-        		if(!dfstrav(i,visited,pathvisit,adjlist)){
-        			return 0;
+            cout<<indeg[i]<<endl;
+        	if(indeg[i]==0){
+        		pending.push(i);
+        	}
+        }
+        while(!pending.empty()){
+        	int len = pending.size();
+        	for(int k=0; k<pending.size(); k++){
+        		int cur = pending.front();
+        		pending.pop();
+        		ans.push_back(cur);
+        		for(int i=0; i<adjlist[cur].size(); i++){
+        			indeg[adjlist[cur][i]]--;
+        			if(indeg[adjlist[cur][i]]==0){
+        				pending.push(adjlist[cur][i]);
+        			}
         		}
         	}
         }
-        return 1;
+        return (numCourses==ans.size());
     }
 };
