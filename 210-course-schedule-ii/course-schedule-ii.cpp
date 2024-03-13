fda5c121845fd883b9edcb7e1767746cba81db50
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-03-13 12:02:17 +0530

    Time: 20 ms (30.35%) | Memory: 16.9 MB (73.00%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> ans;
+        vector<int> indeg(numCourses,0);
+        queue<int> pending;
+        vector<vector<int>> adjlist(numCourses,vector<int>());
+        for(int i=0; i<prerequisites.size(); i++){
+        	adjlist[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for(int i=0; i<adjlist.size(); i++){
+        	for(int j=0; j<adjlist[i].size(); j++){
+        		indeg[adjlist[i][j]]++;
+        	}
+        }
+        for(int i=0; i<numCourses; i++){
+        	if(indeg[i]==0){
+        		pending.push(i);
+        	}
+        }
+        while(!pending.empty()){
+        	int len = pending.size();
+        	for(int k=0; k<pending.size(); k++){
+        		int cur = pending.front();
+        		pending.pop();
+        		ans.push_back(cur);
+        		for(int i=0; i<adjlist[cur].size(); i++){
+        			indeg[adjlist[cur][i]]--;
+        			if(indeg[adjlist[cur][i]]==0){
+        				pending.push(adjlist[cur][i]);
+        			}
+        		}
+        	}
+        }
+        if(ans.size() != numCourses){
+            return {};
+        }
+        return ans;     
+    }
+};
