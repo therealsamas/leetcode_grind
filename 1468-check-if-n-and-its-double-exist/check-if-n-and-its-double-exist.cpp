e3ea2535828908d120bb668c10d14cbe03650f9e
therealsamas <126412384+therealsamas@users.noreply.github.com>
2024-12-01 17:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (41.61%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,13 +1,30 @@
+
 class Solution {
 public:
+
+    bool binsearch(vector<int>& arr, int idx){
+        int lo=0;
+        int hi = arr.size()-1;
+        int val = 2*arr[idx];
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            if(arr[mid] == val && idx != mid){
+                return true;
+            }else if(arr[mid] < val){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return false;
+    }
+
     bool checkIfExist(vector<int>& arr) {
-        unordered_set<int> hashset;
-        for(int i=0; i<arr.size(); i++){
-            if(hashset.find(arr[i]*2) != hashset.end() || 
-                (arr[i]%2==0 && hashset.find(arr[i]/2) != hashset.end())){
+        sort(arr.begin(), arr.end());
+        for (int i = 0; i < arr.size(); i++){
+            if(binsearch(arr,i)){
                 return true;
             }
-            hashset.insert(arr[i]);
         }
         return false;
     }
