21839eae36a450ac4687acebed3c8df877bd708f
therealsamas <126412384+therealsamas@users.noreply.github.com>
2025-03-05 22:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-
-    long long calchelper(int n, vector<long long>& dp){
-        if(n<=1){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-
-        dp[n] = 4*(n-1) + calchelper(n-1,dp);
-        return dp[n];
-    }
-
     long long coloredCells(int n) {
-        vector<long long> dp(n+1,-1);
-        return calchelper(n,dp);
+        long long prev = 1;
+        long long ans = 1;
+        for(int i=1; i<=n; i++){
+            ans = 4*(i-1) + prev;
+            prev = ans;
+        }
+        return ans;
     }
 };
